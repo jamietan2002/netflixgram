ea47a725f56b01a4d06cc8fff48510ec734ee073
jamie tan <jamietan2002@gmail.com>
2022-06-05 00:19:20 +0800

    last

--- netflixgram/netflixgram/settings.py
+++ netflixgram/netflixgram/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'netflixgram.urls'
@@ -140,3 +141,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
